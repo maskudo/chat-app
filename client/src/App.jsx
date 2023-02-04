7363c4c0367b76cbf3b19a351b97758942fc9111
maskudo <manujkarki101@gmail.com>
2023-02-04 14:00:36 +0545

    setup basic routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,18 @@
-import { useState } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Register from './pages/Register.jsx';
+import Login from './pages/Login.jsx';
+import Chat from './pages/Chat.jsx';
 
 function App() {
-  return <div className='App'>hello world</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/register' element={<Register />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/chat' element={<Chat />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;

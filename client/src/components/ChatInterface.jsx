670a9a9685c77504f563f4c23f679c88524715fc
maskudo <manujkarki101@gmail.com>
2023-06-05 23:17:16 +0545

    feat: add emoji input

--- client/src/components/ChatInterface.jsx
+++ client/src/components/ChatInterface.jsx
@@ -1,15 +1,17 @@
-import { faPaperPlane } from '@fortawesome/free-solid-svg-icons';
+import { faFaceSmile, faPaperPlane } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { ToastContainer } from 'react-toastify';
+import EmojiPicker from 'emoji-picker-react';
 import Message from './Message';
 
 export default function ChatInterface({ sendMessage, selectedContact }) {
   const messageContainerRef = useRef(null);
-  const inputRef = useRef(null);
+  const [msg, setMsg] = useState('');
   const currentUser = useSelector((state) => state.user?.user?._id);
   const messages = useSelector((state) => state.message.messages);
+  const [displayEmojiPicker, setDisplayEmojiPicker] = useState(false);
 
   useEffect(() => {
     // Scroll to the bottom of the message container whenever new messages are added
@@ -19,13 +21,21 @@ export default function ChatInterface({ sendMessage, selectedContact }) {
 
   const handleSendMessage = (e) => {
     e.preventDefault();
-    const inputValue = inputRef.current.value;
+
+    const inputValue = msg;
     if (!inputValue) return;
 
     sendMessage(inputValue);
-    inputRef.current.value = '';
+    setMsg('');
   };
-
+  const handleEmojiClick = (emojiObject, event) => {
+    console.log(emojiObject.emoji);
+    setMsg(`${msg}${emojiObject.emoji}`);
+    setDisplayEmojiPicker(false);
+  };
+  const handleDisplayEmojiPicker = () => {
+    setDisplayEmojiPicker(!displayEmojiPicker);
+  };
   return (
     <>
       <div className="chat-container my-0 flex flex-col h-full">
@@ -43,15 +53,32 @@ export default function ChatInterface({ sendMessage, selectedContact }) {
             ))}
         </div>
         {!!selectedContact && (
-          <div className="input-container w-full basis-1/6 pt-4">
+          <div className="input-container relative w-full basis-1/6 pt-4">
+            {displayEmojiPicker && (
+              <div className="absolute bottom-28 left-4">
+                <EmojiPicker onEmojiClick={handleEmojiClick} />
+              </div>
+            )}
             <form
               onSubmit={(e) => handleSendMessage(e)}
               className="mx-4 flex justify-between"
             >
+              <button
+                type="button"
+                className="h-14 w-14 mx-2 text-2xl rounded-full text-white bg-yellow-500"
+              >
+                <div className="button h-auto w-auto rounded-full">
+                  <FontAwesomeIcon
+                    icon={faFaceSmile}
+                    onClick={handleDisplayEmojiPicker}
+                  />
+                </div>
+              </button>
               <input
                 type="text"
                 placeholder="Type your message..."
-                ref={inputRef}
+                onChange={(e) => setMsg(e.target.value)}
+                value={msg}
                 className="text-black h-16 w-5/6 rounded-lg p-4 msg-input"
               />
               <button
